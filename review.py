2ef17f89c7461174d930e411ebc15523caf79579
hasanzaidi1 <hasanammarza@usf.edu>
2023-08-24 14:45:13 -0400

    bitwise

--- review.py
+++ review.py
@@ -18,8 +18,13 @@ print()
 
 
 #Bitwise operators
-print(4 << 2)
-print(128 >> 4)
+print(4 << 2) # 4 / 2^2 = 16
+print(128 >> 4) # 128 / 2^4 = 8
+print(0b111 & 0b1100) # = 0b1100 = 12    and
+print(0b111 | 0b1100) # = 0b1111 = 15    or
+print(0b111 ^ 0b0101) # = 0b1001 = 9     xor
+
+
 
 
 
