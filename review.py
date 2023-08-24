f867753ae93a76cce586f6ae33d11234b3a7f6d2
hasanzaidi1 <hasanammarza@usf.edu>
2023-08-24 14:37:51 -0400

    bitwise and logical

--- review.py
+++ review.py
@@ -1,5 +1,27 @@
 
-print("revision basic python")
+print("\nrevision basic python")
+
+intNum = int(23.99)
+floatNum = float(22.999999999)
+
+print(intNum, floatNum)
+print(intNum, int(floatNum))
+
+#logical 
+
+print()
+print(False and False)
+print(True and False)
+print(True or False)
+print(not False)
+print()
+
+
+#Bitwise operators
+print(4 << 2)
+print(128 >> 4)
 
 
 
+
+print("\n")
