98a615157065847638cb1025c47192214990e504
hasanzaidi1 <hasanammarza@usf.edu>
2023-09-06 11:59:44 -0400

    HW1_Q1

---
+++ HW1_Q1.py
@@ -0,0 +1,34 @@
+
+def read():
+    file = open("inputHW1.txt", "r")
+    line = file.readline()
+    values = []
+    for value in line.split(' '):
+        values.append(int(value))
+    return values
+   
+
+def min(values = read()):
+    min = values[0]
+    for i in range(len(values)):
+        if values[i] < min:
+            min = values[i]
+        else:
+            continue
+        return min
+
+def max(values = read()):
+    max = values[0]
+    for i in range(len(values)):
+        if values[i] > max:
+            max = values[i]
+        else:
+            continue
+        return max
+
+
+minVar = min()
+maxVar = max()
+
+print(minVar)
+print(maxVar)
