5922a7e4f32f5e49310031da96b08d45a1e43939
hasanzaidi1 <hasanammarza@usf.edu>
2023-09-06 11:41:12 -0400

    HW1_Q2

---
+++ HW1_Q2.py
@@ -0,0 +1,17 @@
+
+arr = [1, 4, 9, 11, 8, 3, 2, 5, 10]
+
+num = arr[0]
+
+while True:
+    i = 0
+    if arr[i] > num:
+        num = arr[i]
+        i += 1
+
+    else:
+        print(num)
+        break
+        
+
+
