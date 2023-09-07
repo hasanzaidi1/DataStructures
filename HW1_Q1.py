d49020fc30ed5ee9c96259ef15ab76d6789d4de7
hasanzaidi1 <hasanammarza@usf.edu>
2023-09-06 23:44:28 -0400

    sep_6th

--- HW1_Q1.py
+++ HW1_Q1.py
@@ -28,19 +28,50 @@ def max(values = read()):
 
 
 def Trendchange(values = read()):
+    
     change = list()
-    num = values[0]
-    for i in values:
-        for x in values[1:]:
-            if i < x:
-                print("asc")
-            if i > x:
-                print("dec")
+    
+    index = 0
+
+    for i in values[1:]:
+
+        previous_num = values[index]            # first num
+        if i < previous_num:                        # second num is less than first num
+            # decreasing loop
+            # ISSUE : change the continuing range
+            for num_dec in values[1:]:                  # a loop for numbers decreasing will initalize
+                if num_dec < previous_num:                  # second num is less than first num
+                    index = index + 1                           # increment index by 1
+                    continue                                    # continue the loop
+                else:                                       # else
+                    change.append(num_dec)                      # add that number which is changing the trend to list called change
+                    break                                       # break the loop after adding that number
+
+        elif i > previous_num:                      # second num is greater than first num
+            # increasing loop
+            # ISSUE : change the continuing range
+            for num_inc in values[:1]:                  # a loop for numbers increasing will initalize
+                if num_inc > previous_num:                  # second num is more than first num
+                    index = index + 1                           # increment index by 1
+                    continue                                    # continue the loop
+                else:                                       # else
+                    change.append(num_inc)                      # add that number which is changing the trend to list called change
+                    break                                       # break the loop after adding that number
+
+        else:                                       # else 
+            continue                                    # if both the nums are the same then continue
+
+        
+
+    return change
+
+        
+
 
 trend = Trendchange()
 minVar = min()
 maxVar = max()
 
-# print(trend)
+print(trend)
 print(minVar)
 print(maxVar)
